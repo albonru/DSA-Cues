fd14d6eff2c659e14c1b5c823e931cd5ff62a147
Alba Onrubia <alba.onrubia@estudiantat.upc.edu>
2022-03-22 17:22:49 +0100

    Second version

--- src/edu/upc/eetac/dsa/QueueImpl.java
+++ src/edu/upc/eetac/dsa/QueueImpl.java
@@ -4,36 +4,40 @@ public class QueueImpl<E> implements Queue<E> {
 
     public int max; //max elements allowed in queue
     public E[] queue; //queue of E type
+    public int nelem; //number of elements in array
 
     public QueueImpl (int max) {
         this.max = max;
         this.queue = null; //CONSULTA : A QUE SI NO A NULL??
+        this.nelem = 0;
     }
 
     @Override
     public void push(E e) throws FullQueueException { //CONSULTA: OMPLIR CLASSES EXCEPCIONS??
         try {
-            if (this.queue.length < this.max)
-                this.queue[this.queue.length - 1] = e;
+            if (this.nelem < this.max) {
+                this.queue[this.nelem - 1] = e;
+                ++this.nelem;
+            }
         }
         catch(Exception FullQueueException) {
-            System.out.println("Full Queue");
+            System.out.println("Queue full");
         }
     }
 
     @Override
     public E pop() throws EmptyQueueException { //CONSULTA : TRY-CATCH O IF-ELSE??
         try {
-            if (this.queue.length != 0) {
+            if (this.nelem != 0) {
                 E popped = this.queue[0];
                 this.queue[0] = null;
-                for (int i = 0; i<this.queue.length; i++) //CONSULTA : CONFIRMAR NO UN FOR-EACH??
+                for (int i = 0; i<this.nelem; i++) //CONSULTA : CONFIRMAR NO UN FOR-EACH??
                     this.queue[i] = this.queue[i+1];
                 return popped;
             }
         }
         catch(Exception EmptyQueueException) {
-            System.out.println("Empty Queue");
+            System.out.println("Queue empty");
         }
         return null; //CONSULTA : DEMANA RETURN, NULL??
     }
